7dae11237ae36a040aaf4a8ab505c056d283bff6
Hauzer S. Lee <harrozze@gmail.com>
2023-07-10 18:28:42 +0800

    src/utils/base64.js added, to export methods from js-base64 package

---
+++ src/utils/base64.js
@@ -0,0 +1,9 @@
+import {Base64} from 'js-base64'
+
+export const base64_encode = Base64.enode
+export const base64_decode = Base64.decode
+
+export default {
+    base64_encode: base64_encode,
+    base64_decode: base64_decode
+}
